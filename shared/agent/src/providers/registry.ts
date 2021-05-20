67679c12be5318a11a24d89cdf7f4c970a5a532f
Marcelo Bukowski de Farias <marcelo@codestream.com>
2021-05-20 15:38:30 -0400

    Configure provider on connect

--- shared/agent/src/providers/registry.ts
+++ shared/agent/src/providers/registry.ts
@@ -295,6 +295,11 @@ export class ThirdPartyProviderRegistry {
 			throw new Error(`No registered provider for '${request.providerId}'`);
 		}
 		await provider.connect();
+		await provider.configure({
+			token: provider.accessToken,
+			data: {}
+		});
+
 		return {};
 	}
 
